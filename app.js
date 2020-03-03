91eff50ed2cb22ab6fa0a1bd03ccc28397799d4e
OhSung Lee <Ostin@OSTIN-PC.EHQ.AD>
2020-03-03 07:25:51 +0000

    initial

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
@@ -9,46 +8,32 @@ var total_queue_gateway = [];
 var total_queue_main = [];
 
 const max_buffer = 2;
-var con_orgin;
+var conn_orgin;
 
 var created_socket_id;
 
 app.use(express.static("public"));
 
-app.use(session({
-	secret: 'secret-key',
-	resave: true,
-    saveUninitialized: true
-}));
-
-const auth = function(req, res, next){
-    if (req.session.loggedin)
-        return next();
-    else
-		res.redirect('/');
-};
-
-app.post('/session_close', function (req, res){
-    req.session.destroy();
-    res.end();
-});
-
 io.on('connection', (socket) => {
 
-    con_orgin = socket.handshake.headers.referer;
+    conn_orgin = socket.handshake.headers.referer;
     
     created_socket_id = socket.id;
     
-    if(con_orgin==="http://localhost:3000/"){
+    if(conn_orgin==="http://localhost:3000/"){
         total_queue_gateway.push(created_socket_id);
     } else {
-        total_queue_main.push(created_socket_id);
+        if(total_queue_main.length >= max_buffer){
+            io.to(created_socket_id).emit('unauthorized');
+        } else {
+            total_queue_main.push(created_socket_id);
+        }
     }
 
-    io.to(created_socket_id).emit('init', total_queue_gateway.length + max_buffer);
+    io.to(created_socket_id).emit('init', total_queue_gateway.length + total_queue_main.length - max_buffer);
 
     if(max_buffer - total_queue_main.length > 0){
-        io.to(created_socket_id).emit('redirect');
+        io.to(created_socket_id).emit('forward');
     } else {
         io.to(created_socket_id).emit('wait');
     }
@@ -61,7 +46,7 @@ io.on('connection', (socket) => {
 
         if(total_queue_main.indexOf(socket.id)>-1){
 
-            io.to(total_queue_gateway.shift()).emit('redirect');
+            io.to(total_queue_gateway.shift()).emit('forward');
 
             for(let i = 0; i < total_queue_gateway.length; i++){
                 if(total_queue_gateway[i]!==undefined){
@@ -73,7 +58,6 @@ io.on('connection', (socket) => {
         total_queue_main = total_queue_main.filter(function(item) {
             return item !== socket.id;
         }); 
-
     });       
                 
 });
@@ -82,12 +66,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/html/gateway.html'));
 });
 
-app.post('/auth', function(req, res) {
-    req.session.loggedin = true;
-    res.redirect('/target');
-});
-
-app.get('/target', auth, function(req, res) {
+app.get('/target', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/html/target.html'));
 });
 
